25fbd0b49331bd624497159e48c95b48b5386f43
Aarav <aamande2@illinois.edu>
2023-01-29 00:35:20 +0000

    implement encoder

--- part1.py
+++ part1.py
@@ -1,6 +1,32 @@
+import numpy as np
 
 def encoder(image):
-    pass
+    image_ = image.reshape(len(image)*len(image))
+    size = len(image_)
+    n_ = np.ceil(np.log(len(image)) / np.log(2))
+    n_qubits = (2*n_) + 1
+    q = qiskit.QuantumRegister(3)
+    qc = qiskit.QuantumCircuit(n_qubits)
+    for i in range(n_qubits - 1):
+        qc.h(i)
+    qc.barrier()
+    for i in range(2**(2*n_)):
+        for j in range(n_qubits - 1):
+            b = format(j, f'0{n_}b')
+            for l in len(b):
+                if b[l] == '0':
+                    qc.x(l)
+
+        qc.mcry(image_[len - i], np.arange(n_qubits - 1), n_qubits-1)
+        
+        
+        for j in range(n_qubits - 1):
+            b = format(j, f'0{n_}b')
+            for l in len(b):
+                if b[l] == '0':
+                    qc.x(l)
+        qc.barrier()
+    return qc
 
 def decoder(histogram):
     # count up in binary up to number of pixels
